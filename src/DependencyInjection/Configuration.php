cf0ef020ca5dc2004177ccdcbf890c8a3d7880b6
Jakob Schumann <114239+j-schumann@users.noreply.github.com>
2020-08-02 12:42:51 +0200

    add: empty config class

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vrok\SymfonyAddons\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('vrok_symfony_addons');
+
+        return $treeBuilder;
+    }
+}
