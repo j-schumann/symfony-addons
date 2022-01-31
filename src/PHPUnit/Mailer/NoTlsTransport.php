bc7a2f890db67f87e4ddbec57974ae08761a63b8
j-schumann <114239+j-schumann@users.noreply.github.com>
2022-01-31 18:03:16 +0100

    fix: notls transport

--- src/PHPUnit/Mailer/NoTlsTransport.php
+++ src/PHPUnit/Mailer/NoTlsTransport.php
@@ -37,35 +37,6 @@ class NoTlsTransport extends EsmtpTransport
         ];
     }
 
-    public function setUsername(string $username): self
-    {
-        $this->username = $username;
-
-        return $this;
-    }
-
-    public function getUsername(): string
-    {
-        return $this->username;
-    }
-
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    public function getPassword(): string
-    {
-        return $this->password;
-    }
-
-    public function addAuthenticator(AuthenticatorInterface $authenticator): void
-    {
-        $this->authenticators[] = $authenticator;
-    }
-
     protected function doHeloCommand(): void
     {
         try {
