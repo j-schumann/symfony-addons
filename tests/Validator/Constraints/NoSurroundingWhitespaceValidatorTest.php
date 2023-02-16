678057546f6a75db6dad04734c6806bd5d13ed1b
j-schumann <114239+j-schumann@users.noreply.github.com>
2023-02-16 11:08:01 +0100

    fix: NoSurroundingWhitespace w/ linebreaks

--- tests/Validator/Constraints/NoSurroundingWhitespaceValidatorTest.php
+++ tests/Validator/Constraints/NoSurroundingWhitespaceValidatorTest.php
@@ -15,30 +15,55 @@ class NoSurroundingWhitespaceValidatorTest extends ConstraintValidatorTestCase
         return new RegexValidator();
     }
 
-    public function getValid()
+    public function getValid(): array
     {
         return [
             ["test\tstring"], // tab inside
             ['.0 d'], // space inside
             ['*tes t'],
             ['_as d@'],
+
+            // make sure linebreaks within the string are allowed:
+            ["valid\nmultiline"],
+            ["valid\n space multiline"],
+            ["valid\r\nwindows multiline"],
+            ["new\x0bline"], // vertical tab
+            ["new\xc2\x85line"], // NEL, Next Line
+            ["test\vspace"], // vertical space
+            ["new\xe2\x80\xa8line"], // Unicode LS
+            ["new\xe2\x80\xa9line"], // Unicode PS
         ];
     }
 
-    public function getInvalid()
+    public function getInvalid(): array
     {
         return [
-            [" asd", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'],
-            ["asd  ", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'],
+            // whitespace character at the beginning or end:
+            [' asd', 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'],
+            ['asd  ', 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'],
             ["\tasd", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'],
             ["asd\t", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'],
-            [" asd", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // THSP leading
-            ["asd ", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // THSP trailing
-            [" asd", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // NQSP
-            [" asd", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // MQSP
-            ["asd ", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // ENSP
-            ["asd ", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // EMSP
-            ["asd ", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // 3/MSP
+            [' asd', 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // THSP leading
+            ['asd ', 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // THSP trailing
+            [' asd', 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // NQSP
+            [' asd', 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // MQSP
+            ['asd ', 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // ENSP
+            ['asd ', 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // EMSP
+            ['asd ', 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // 3/MSP
+
+            // leading/trailing newline characters:
+            ["a\nb\n", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // trailing newline
+            ["\na\nb", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // leading newline
+            ["new\x0b", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // vertical tab
+            ["\x0btest", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // vertical tab
+            ["new\xc2\x85", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // NEL, Next Line
+            ["\xc2\x85test", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // NEL, Next Line
+            ["test\v", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // vertical space
+            ["\vtest", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // vertical space
+            ["new\xe2\x80\xa8", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // Unicode LS
+            ["\xe2\x80\xa8test", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // Unicode LS
+            ["new\xe2\x80\xa9", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // Unicode PS
+            ["\xe2\x80\xa9test", 'de1e3db3-5ed4-4941-aae4-59f3667cc3a3'], // Unicode PS
         ];
     }
 
